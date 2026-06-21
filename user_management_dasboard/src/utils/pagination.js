c3c0a94f12e96f5983c2946badd8d6a0e68923a4
anubhav maurya <channelsub47@gmail.com>
2026-06-21 19:07:48 +0530

    feat: add reusable pagination

--- user_management_dasboard/src/utils/pagination.js
+++ user_management_dasboard/src/utils/pagination.js
@@ -1,5 +1,5 @@
-export const paginate = (data, currentPage, pageSize) => {
-  const start = (currentPage - 1) * pageSize;
+export const paginate = (items, currentPage, pageSize) => {
+  const startIndex = (currentPage - 1) * pageSize;
 
-  return data.slice(start, start + pageSize);
+  return items.slice(startIndex, startIndex + pageSize);
 };
