7a020804ddc6226ba77fbf2031d54bc726d9003a
anubhav maurya <channelsub47@gmail.com>
2026-06-21 19:13:00 +0530

    feat: configure global toast notifications

--- user_management_dasboard/src/App.jsx
+++ user_management_dasboard/src/App.jsx
@@ -1,13 +1,20 @@
-import { useState } from 'react'
+import { Toaster } from "react-hot-toast";
+
+import Dashboard from "./pages/Dashboard";
 
 function App() {
-
   return (
     <>
-    
-      
+      <Toaster
+        position="top-right"
+        toastOptions={{
+          duration: 3000,
+        }}
+      />
+
+      <Dashboard />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
