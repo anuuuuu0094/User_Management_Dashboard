87e358fab260613e13ee619c0dd5eaab90e85008
anubhav maurya <channelsub47@gmail.com>
2026-06-21 18:58:45 +0530

    feat: add centralized axios configuration

--- user_management_dasboard/src/api/axios.js
+++ user_management_dasboard/src/api/axios.js
@@ -2,7 +2,22 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://jsonplaceholder.typicode.com",
-  timeout: 10000
+  timeout: 10000,
+  headers: {
+    "Content-Type": "application/json"
+  }
 });
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    const message =
+      error.response?.data?.message ||
+      error.message ||
+      "Something went wrong";
+
+    return Promise.reject(new Error(message));
+  }
+);
 
 export default api;
